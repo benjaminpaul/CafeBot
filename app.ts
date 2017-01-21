aa596d90f0523169d795822e3951c5f830ab46e0
Benjamin Paul <benjaminpaul@Benjamins-MacBook-Pro.local>
2017-01-21 12:48:56 +0000

    Added card welcome response

--- app.ts
+++ app.ts
@@ -24,13 +24,22 @@ bot.dialog("/", intents);
 
 intents.matches("Greeting", [
     (session, args, next) => {
-        session.send("Hello, welcome to " + process.env.CAFE_NAME + ", How can I help?");
+        var card = new builder.HeroCard(session)
+        .title(process.env.CAFE_NAME)
+        .subtitle("126 Church Road, Bristol.")
+        .text("Hello there, thanks for getting in touch! - What can I help you with?")
+        .buttons([
+            builder.CardAction.postBack(session, "What are you open?", "Show Opening Times")
+        ]);
+
+        var message = new builder.Message(session).addAttachment(card);
+        session.send(message);
     }
 ]);
 
 intents.matches("OpeningTimes", [
     (session, args, next) => {
-        session.send("Let just grab our opening times for you...");
+        session.send("Here are our opening times for the week...");
         var s = "";
         data.openingTimes.forEach((openingTime) => {
             s = s + openingTime.dayOfWeek + ": " + openingTime.openFrom + " - " + openingTime.openTo + "\n\n";
