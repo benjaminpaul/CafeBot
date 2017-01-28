95aee5167b6e487c95216cb590561ff7deb7f07f
Benjamin Paul <benjaminpaul@Benjamins-MacBook-Pro.local>
2017-01-28 13:11:47 +0000

    Changes for Cash4Clothes

--- app.ts
+++ app.ts
@@ -37,20 +37,21 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-var model = process.env.LUIS_MODEL || "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/be63655b-3463-453c-8c86-2ff01f190a59?subscription-key=cd56d7b3af9240a5a2fb21dac2ae2bc3&verbose=true";
+var model = process.env.LUIS_MODEL || "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/d8e9d06f-a80c-4887-983d-fd83594a6711?subscription-key=cd56d7b3af9240a5a2fb21dac2ae2bc3&verbose=true";
 var recognizer = new builder.LuisRecognizer(model);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 bot.dialog("/", intents);
 
 
+// Greeting.
 intents.matches("Greeting", [
     (session, args, next) => {
-        session.send("Hello there! Welcome to " + process.env.CAFE_NAME + ", I am a friendly chat bot who can help you get information or make orders.\n\n")
+        session.send("Hello there! Welcome to Cash 4 Clothes, I am a friendly chat bot who can help you find out about our services or even arrange a collection or delivery..\n\n")
         var card = new builder.HeroCard(session)
         .title("How can I help?")
         .buttons([
-            builder.CardAction.postBack(session, "What are your opening times?", "Show Opening Times"),
-            builder.CardAction.postBack(session, "I would like to make an order please?", "Order Food"),
+            builder.CardAction.postBack(session, "Do you collect from my postcode?", "Do you collect from my area?"),
+            builder.CardAction.postBack(session, "Nearest outlet", "Where is your nearest outlet?"),
             builder.CardAction.postBack(session, "What is your phone number?", "Call Us")
         ]);
 
@@ -59,74 +60,11 @@ intents.matches("Greeting", [
     }
 ]);
 
-intents.matches("OpeningTimes", [
+// Contact Details.
+intents.matches("ContactDetails", [
     (session, args, next) => {
-        session.send("Here are our opening times for the week...");
-        var s = "";
-        data.openingTimes.forEach((openingTime) => {
-            s = s + openingTime.dayOfWeek + ": " + openingTime.openFrom + " - " + openingTime.openTo + "\n\n";
-        });
-
-        session.send(s);
-    }
-]);
-
-intents.matches("IsOpen", [
-    (session, args, next) => {
-        var date = new Date();
-        var day = date.getDay();
-        var hours = date.getHours();
-
-        var openingTime = data.openingTimes[day];
-        if (openingTime) {
-            session.send("Here are our opening times for today...");
-            session.send(openingTime.dayOfWeek + ": " + openingTime.openFrom + " - " + openingTime.openTo);
-        }
-    }
-])
-
-intents.matches("TakeOrder", [
-    (session, args, next) => {
-        builder.Prompts.text(session, "What would you like to order?");
-    },
-    (session, results, next) => {
-        if (results.response)  {
-            session.send("Thanks, I have sent that order to our staff and it will be ready for you in around 30 minutes.")
-            sendEmail("ben.paul@just-eat.com", results.response);
-        } else {
-            next();
-        }
-    }
-]);
-
-intents.matches("PhoneNumber", [
-    (session, args, next) => {
-        session.send("Our phone number is: 07982 628 199");
-    }
-]);
-
-intents.matches("SendMessage", [
-    (session, args, next) => {
-        builder.Prompts.text(session, "No problem, I can pass that on for you, what is it you wanted to say?");
-    },
-    (session, results, next) => {
-        if (results.response) {
-            session.send("Thanks, I have passed that information to our staff.");
-        } else {
-            next();
-        }
-    }
-])
-.onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said, please try again."));
-
-intents.matches("Thankyou", [
-    (session, args, next) => {
-        session.send("No problem at all.");
-    }
-])
-
-intents.matches("HowAreYou", [
-    (session, args, next) => {
-        session.send("I'm great! Thanks for asking :)");
+        session.send("Our contact details are:");
+        session.send("Outside of Bristol:\n\nCall: 01708 689 985\n\nText: 07519 774 277");
+        session.send("Bristol Area:\n\nCall: 01708 689 984\n\nText: 07514 030 630");
     }
 ]);
--- data.ts
+++ data.ts
@@ -4,6 +4,11 @@ export interface IOpeningTime {
     openTo: string;
 }
 
+export interface ICollectionDay {
+    postcodes: string[];
+    collectionDay: string;
+}
+
 export const openingTimes:Array<IOpeningTime> = [
     { dayOfWeek: "Monday", openFrom: "9:00", openTo: "17:00" },
     { dayOfWeek: "Tuesday", openFrom: "9:00", openTo: "17:00" },
@@ -13,3 +18,30 @@ export const openingTimes:Array<IOpeningTime> = [
     { dayOfWeek: "Saturday", openFrom: "9:00", openTo: "17:00" },
     { dayOfWeek: "Sunday", openFrom: "9:00", openTo: "17:00" },
 ];
+
+export const collectionDays:Array<ICollectionDay> = [
+    { 
+        collectionDay: "Monday",
+        postcodes: ["BS20", "BS21", "BS22", "BS23", "BS24", "BS25", "BS26", "BS27", "BS28", "BS29", "BS40", "BS41", "BS48", "BS49", "TA1", "TA2", "TA3", "TA4", "TA5", "TA6", "TA7", "TA8", "TA9"],
+    },
+    {
+        collectionDay: "Tuesday",
+        postcodes: ["BS35", "BS36", "BS37", "GL5", "GL6", "GL8", "GL9", "GL10", "GL11", "GL12", "GL13"]
+    },
+    {
+        collectionDay: "Wednesday",
+        postcodes: ["BS39", "BA1", "BA2", "BA3", "BA4", "BA5", "BA6", "BA7", "BA8", "BA9", "BA10", "BA16"]
+    },
+    {
+        collectionDay: "Thursday",
+        postcodes: ["BA11", "BA12", "BA13", "BA14", "BA15"]
+    },
+    {
+        collectionDay: "Friday",
+        postcodes: ["SN10", "SN11", "SN12", "SN13", "SN14", "SN15"]
+    },
+    {
+        collectionDay: "Tuesday to Friday",
+        postcodes: ["BS1", "BS2", "BS3", "BS4", "BS5", "BS6", "BS7", "BS8", "BS9", "BS10", "BS11", "BS13", "BS14", "BS15", "BS16", "BS30", "BS31", "BS32", "BS34"]
+    }
+]
