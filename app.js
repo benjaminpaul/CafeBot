82390997363823d88448c478ac8ca5f3200c3cad
Ben Paul <ben.paul@just-eat.com>
2017-02-08 11:33:53 +0000

    Whpo

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ intents.matches("None", [
 intents.matches("Greeting", [
     function (session, args, next) {
         session.userData = null;
-        session.send("Hello there! Welcome to Cash 4 Clothes, I am a friendly chat bot who can help you find out about our services or even arrange a collection or delivery..\n\n");
+        session.send("Hello :) Welcome to Cash 4 Clothes, I am a chat bot who is here to help you.\n\nIf you get stuck, just ask for help at any time :)");
         var card = new builder.HeroCard(session)
             .title("How can I help?")
             .buttons([
-            builder.CardAction.postBack(session, "Do you collect from my postcode?", "Do you collect from my area?"),
-            builder.CardAction.postBack(session, "Nearest outlet", "Where is your nearest outlet?"),
-            builder.CardAction.postBack(session, "What is your phone number?", "Call Us")
+            builder.CardAction.postBack(session, "I want you to collect some clothes", "I have clothes for you"),
+            builder.CardAction.postBack(session, "Nearest outlet", "Where are your shops?"),
+            builder.CardAction.postBack(session, "What do you accept", "What do you accept")
         ]);
         var message = new builder.Message(session).addAttachment(card);
         session.send(message);
