1413aec5cb6ef811d13e7cfaf5939409bc734557
Ben Paul <ben.paul@just-eat.com>
2017-02-14 13:20:45 +0000

    Hello

--- services/email-service.ts
+++ services/email-service.ts
@@ -4,10 +4,15 @@ export class EmailService {
     public sendAppointmentEmail(appointment) {
         var sg = sendgrid(process.env.SENDGRID);
         
+        var message = "Collect On: " + appointment.collectionDay + "\n\n"
+            + "From Address: " + appointment.address + "\n\n"
+            + "Contact Number: " + appointment.contactNumber + "\n\n"
+            + "Funds 4 Schools: " + appointment.funds4schools
+
         var to = new sendgrid.mail.Email("ben.paul@just-eat.com");
         var from = new sendgrid.mail.Email("benjaminpaul1984@googlemail.com");
         var subject = "Scheduled Appointment from Bot"
-        var content = new sendgrid.mail.Content('text/plain', 'Hello, Email!');
+        var content = new sendgrid.mail.Content('text/plain', message);
         var mail = new sendgrid.mail.Mail(from, subject, to, content);
 
         var r = sg.emptyRequest();
